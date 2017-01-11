5495d97689a46fe4c68138efe48da8dc511ad608
Alex Tan <alexst@uw.edu>
2017-01-10 18:20:39 -0800

    Complete the practice for module 5

---
+++ Practice.R
@@ -0,0 +1,16 @@
+2+0+1
+10000-8
+2^10
+print("hello World")
+x <- 201
+x <- 4
+201 -> x
+a <- 1
+b <- 2
+c <- 3
+d <- 4
+paste("Hello", "world", sep=" ")
+test <- round(5/7, 2)
+?round
+print("help")
+"help"
